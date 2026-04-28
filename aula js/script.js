6d289c0ca31ebf10b544da7f4fe798e16f38619c
DaviLamaison <davilamaison66@gmail.com>
2026-04-28 15:46:51 -0300

    Add files via upload

---
+++ aula js/script.js
@@ -0,0 +1,129 @@
+function caixinha (){
+    alert("Olá mundo");
+    let nome = "Davi Lamaison Morais";
+    alert (nome);
+    let number1 = 40;
+    let number2 = 27;
+    let sum = number1 + number2;
+    alert(sum);
+}
+
+/*Exercício Médios*/
+
+function othertext  () {
+    document.getElementById("textinho").textContent = "Mas prefiro Trash Metal"
+}
+
+function color () {
+    document.getElementById("diva").style.background = "red"
+}
+
+function paragraph () {
+    document.getElementById("newp").textContent = "Parabéns";
+}
+
+function exit () {
+    document.getElementById("bad").textContent = "";
+}
+
+function funcao() {
+    let par = document.getElementById("paragrafo");
+    let cax = document.getElementById("caixa")
+    let escrita = par.textContent;
+    cax.value = escrita;
+}
+
+function soma() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let resultado = Number(num1) + Number(num2);
+    let local = document.getElementById("result");
+    local.textContent = resultado;
+}
+
+function sixseven() {
+    document.body.style.background = "#ff00eeee"
+}
+
+function tamanhobb() {
+    document.getElementById("ababua").style.fontSize = "400px";
+}
+
+function sevensix() {
+    document.getElementById("colorsixseven").style.color = "blue";
+}
+function mude1six() {
+    let mudeo6 = document.getElementById("churros7").textContent = "7";
+    let mudeo7 = document.getElementById("churros6").textContent = "6";
+}
+
+function mudeos3() {
+    let mude1 = document.getElementById("sigma1").textContent = "Forja a ética que sustenta nossas escolhas.";
+    let mude2 = document.getElementById("sigma2").textContent = "Molda a virtude que guia nosas ações no mundo.";
+    let mude3 = document.getElementById("sigma3").textContent = "Estrutura a moral que fundamenta nossas escolhas.";
+
+}
+
+/*Exercício Difícil*/
+
+function tct() {
+    let tct1 = document.getElementById("eoe").textContent = "Boa KKKKKK";
+    let tct2 = document.getElementById("eoe").style.color = "blue";
+    let tct3 = document.getElementById("eoe").style.fontSize = "600px";
+}
+
+function valores() {
+    let txt3 = document.getElementById("xc");
+    let txt4 = document.getElementById("cx");
+    let txt5 = document.getElementById("valoress").textContent;
+    txt3.value = txt5;
+    txt4.value = txt5;
+}
+
+function texttot() {
+    document.getElementById("tot").textContent = "Gestor tem muita aura mano, imcomparável";
+}
+
+function colortot() {
+    document.getElementById("tot").style.color = "blue"
+}
+
+function tamtot() {
+    document.getElementById("tot").style.fontSize = "400px"
+}
+
+function twodiv() {
+    document.getElementById("midnight").style.color = "red";
+    document.getElementById("midnight").style.fontSize = "300px";
+    document.getElementById("callifornia").style.fontSize = "300px";
+    document.getElementById("callifornia").style.color = "red";
+}
+
+function gemini() {
+    document.getElementById("oxeksks");
+    document.getElementById("raphael").textContent = oxeksks.value;
+    document.getElementById("raphael").style.color = "green"; 
+}
+
+function som() {
+    let number1 = document.getElementById("chaves").value;
+    let number2 = document.getElementById("quico").value;
+    let resultado = Number(number1) + Number(number2)
+    let local = document.getElementById("srmadruga");
+    local.textContent = resultado;
+}
+
+function aviao() {
+    document.getElementById("srmadruga").textContent = "";
+}
+
+function comandante() {
+    document.getElementById("bad").textContent = "To zoando, é legal sim";
+}
+
+function comandante() {
+    document.getElementById("diva").textContent = "Agora é azul haha";
+    document.getElementById("diva").style.color = "white";
+    document.getElementById("diva").style.background = "blue";
+
+}
